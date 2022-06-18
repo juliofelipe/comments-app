0b1224a6c45a5631ac712f0fc7908882e8403f51
Julio Felipe <julio@informach.com.br>
2022-06-18 11:35:22 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './Post';
+
+import styles from './App.module.css';
+
+import './global.css'
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Julio Felipe" 
+            content="Lorem ipsum color"
+          />
+          <Post 
+            author="Helena Oliveira" 
+            content="Um post dela"
+          />
+        </main>
+      </div>
+    </div>
+    
+  )
+}
+
