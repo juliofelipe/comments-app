b07cbd230c49112b25bf8f0e1ec307a0ebcf34fb
Julio Felipe <julio@informach.com.br>
2022-06-19 13:47:29 -0300

    feat: adding validation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/juliofelipe.png',
+      name: 'Julio Felipe',
+      role: 'CTO @ Informach'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfa.'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/oliveirahelena.png',
+      name: 'Julio Felipe',
+      role: 'CTO @ Informach'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera2'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfa2.'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -14,14 +45,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Julio Felipe" 
-            content="Lorem ipsum color"
-          />
-          <Post 
-            author="Helena Oliveira" 
-            content="Um post dela"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/oliveirahelena.png" />
@@ -15,12 +20,12 @@ export function Comment() {
                             <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash  size={24}/>
                         </button>
                     </header>
 
-                    <p>Muito bom Júlio, parabéns!!</p>
+                    <p>{ content }</p>
                 </div>
 
                 <footer>
